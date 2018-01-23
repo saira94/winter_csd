24e9c7cb56528bfc69566433562cc8ad35ae9bec
saira94 <preotasherpa@gmail.com>
2018-01-23 14:21:12 +0000

    it does not work in ubuntu

--- HW1/magicsquare.cc
+++ HW1/magicsquare.cc
@@ -1,53 +1,50 @@
 // magicsquare for odd number 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main () {
+
+void magicsquare(vector<vector<int> > &matrix, int n);
+
+int main(int argc; char* argv[]) {
 	int n;
 	cout << "enter an odd number";
 	cin  >> n;
-
-	// algorithm to have odd sized magic sqaure
-
-	int magicArry [n][n];
-
-// initialize all values with 0;
-
-	for (int i = 0; i < n; i++) 
-		for (int j = 0; j <n; j++) {
-			magicArry[i][j] = 0;
-		}
-// start positon with 1
-	int nsqr = n * n;
-	int i = 0;
-  int j = n/2;
-
-	for (int k=1; k <nsqr; k++) {
-		magicArry[i][j] = k;
-		i--;
-		j++;
-
-		if (magicArry[i][j] !== 0) {
-			i++;
-			j;
-		}
-		else {
-			if (j == n)
-				j -=n;
-			else if (i<0)
-				i += n;
-		}
-
-	}
-	
-// printing 
-	for(i=0; i<n; i++) {
-		for(j=0;j<n;j++)
-			cout << magicArry[i][j];
-			cout << endl;
-
-	}
+	vector<vector<int> > matrix(n, vector<int> (n,0));:
+	magicsquare(matrix, n);
 
 	return 0;
+
+}
+
+
+void magicsquare(vector<vector<int> > &matrix, int n) {
+	int nsqr = n * n;
+	// start position with 1
+	int i=0;
+	int j= n/2;
+	for(int k=1, k<=nsqr; k++) {
+		matrix[i][j]= k;
+		i--;
+		j++;
+		if(k%n == 0){ // if the place is aldready taken
+			i = i+2;
+			--j;
+		}
+		else {
+			if(j==n)  // if i or j value is outsid of box
+				j -= n;  
+			else if(i<0)
+				i +=n;
+		}
+	}
+
+	//printing
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j <n; j++) {
+			cout << matrix[i][j] <<"  ";	}
+		cout << endl;
+
+   }
 }
